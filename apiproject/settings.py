f8f9752da269ceb72e77e1090cdde022846379d7
Olamidun <kolapoolamidun@gmail.com>
2021-12-01 12:21:55 +0100

    removed unused docstrings

--- apiproject/settings.py
+++ apiproject/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = os.getenv('DJANGO_DEBUG_VALUE', False)
+# DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG_VALUE', False)
 
 ALLOWED_HOSTS = ['localhost', 'c2c-orderapi.herokuapp.com']
 
